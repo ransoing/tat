f93f2d550686cac15f19d758fe7825ec1e05b9ec
Ransom Christofferson <ransom.christofferson@lasp.colorado.edu>
2018-08-31 17:16:15 -0600

    Custom fonts, colors, other styles. Added text

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage implements OnInit {
+
+  ngOnInit() {
+    setTimeout( () => {
+      let tabbar = document.querySelector('ion-tabs').shadowRoot.querySelector('ion-tabbar').shadowRoot;
+      let btns = tabbar.querySelectorAll('.tab-btn');
+      //@ts-ignore
+      btns.forEach( (btn, i) => {
+        //btn.querySelector('ion-icon').shadowRoot.querySelector('.icon-inner').innerHTML = icons[i];
+      });
+    }, 3000 );
+  }
+}

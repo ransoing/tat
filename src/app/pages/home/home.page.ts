44129895bdf5b4d0a906eba2d67d7e40af780110
Ransom Christofferson <ransoing@gmail.com>
2019-04-27 23:21:02 -0600

    Added content warning

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
-import { SettingsService, MiscService } from '../../services';
+import { SettingsService, MiscService, TrxService } from '../../services';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { NotificationType, INotificationData } from '../../models/notification';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -13,13 +14,18 @@ import { NotificationType, INotificationData } from '../../models/notification';
 })
 export class HomePage implements AfterViewInit {
 
+  private readonly doNotShowContentWarningStorageKey = 'doNotShowContentWarning';
+
   constructor(
     public navCtrl: NavController,
     public translate: TranslateService,
     public settings: SettingsService,
     public miscService: MiscService,
     public splashScreen: SplashScreen,
-    public notifications: LocalNotifications
+    public notifications: LocalNotifications,
+    private trx: TrxService,
+    private alertController: AlertController,
+    private storage: Storage
   ) {
     // setTimeout( () => {
     //   this.notifications.getScheduledIds().then( (scheduledIds) => {
@@ -64,6 +70,30 @@ export class HomePage implements AfterViewInit {
     //     }
     //   });
     // }, 2000 );
+
+    // wait one second before triggering the warning
+    setTimeout( () => this.triggerContentWarning(), 1000 );
+  }
+
+  async triggerContentWarning() {
+    // show a content warning, unless the user has chosen not to see the warning
+    let avoidWarning = await this.storage.get( this.doNotShowContentWarningStorageKey );
+    if ( !avoidWarning ) {
+      const alert = await this.alertController.create({
+        message: await this.trx.t( 'home.contentWarning' ),
+        buttons: [{
+          text: await this.trx.t( 'misc.buttons.doNotShow'),
+          handler: () => {
+            // save to storage that we should avoid this warning
+            this.storage.set( this.doNotShowContentWarningStorageKey, true );
+          }
+        }, {
+          text: await this.trx.t( 'misc.buttons.continue'),
+          role: 'cancel'
+        }]
+      });
+      alert.present();
+    }
   }
 
   onSetLanguage() {

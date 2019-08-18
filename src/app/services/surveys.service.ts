1e0c3e22c9ad56832e9d03092e6d9ea6faf37bd0
Ransom Christofferson <ransoing@gmail.com>
2019-08-18 16:47:48 -0600

    Changes for post-outreach

--- src/app/services/surveys.service.ts
+++ src/app/services/surveys.service.ts
@@ -291,9 +291,6 @@ export class SurveyService {
           options: [{
             value: 'willUseTatTraining',
             labelTranslationKey: 'volunteer.forms.postOutreach.labels.cdlSchool.willUseTatTraining'
-          }, {
-            value: 'willPassOnInfo',
-            labelTranslationKey: 'volunteer.forms.postOutreach.labels.cdlSchool.willPassOnInfo'
           }]
         }, {
           type: SurveyFieldType.TEXTAREA,

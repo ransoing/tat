6fe06aad0f14a69248261b74b183260ec80249b3
Ransom Christofferson <ransoing@gmail.com>
2019-12-04 23:55:06 -0700

    fix default notification icon, add app version to About page

--- src/app/modals/about-tat/about-tat.component.ts
+++ src/app/modals/about-tat/about-tat.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MiscService } from '../../services';
+import pack from '../../,./../../../package.json';
 
 @Component({
   templateUrl: './about-tat.component.html'
@@ -8,6 +9,7 @@ export class AboutTatComponent {
 
   public modal: HTMLIonModalElement;
   public lastYear = ( new Date().getFullYear() ) - 1;
+  public version = pack.version;
   
   constructor( public miscService: MiscService ) { }
 
